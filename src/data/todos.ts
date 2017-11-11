78855cff55e274ac14859a08ae098b8b55bdcdc0
ysera34 <ycyoon34@gmail.com>
2017-11-11 11:44:26 +0900

    data todos

---
+++ src/data/todos.ts
@@ -0,0 +1,32 @@
+export default [
+  {
+      id: 1,
+      title: 'php',
+      decsription: 'codeignitor',
+      complete: false,
+  },
+  {
+      id: 2,
+      title: 'react',
+      decsription: 'reactJS',
+      complete: false,
+  },
+  {
+      id: 3,
+      title: 'angular2-5',
+      decsription: 'angularJS5',
+      complete: false,
+  },
+  {
+      id: 4,
+      title: 'typescript',
+      decsription: 'ms에서 만들다니.',
+      complete: false,
+  },
+  {
+      id: 5,
+      title: 'ionic',
+      decsription: '벌써 3까지 나왔네...',
+      complete: false,
+  },
+];
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import todos from '../../data/todos';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  todos: {
+    id: number,
+    title: string,
+    decsription: string,
+    complete: boolean,
+  }[];
   constructor(public navCtrl: NavController) {
 
   }
